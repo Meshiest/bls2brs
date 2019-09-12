a0ccc436b43f1222a52cb78700804d9bff8cf5b1
Meshiest <meek.mesh@gmail.com>
2019-09-12 17:17:46 -0500

    Min viable product. functional brs conversion in browser

--- js/app.js
+++ js/app.js
@@ -5,19 +5,71 @@ const $ = document.querySelector.bind(document);
 
 
 document.addEventListener('DOMContentLoaded', async () => {
-  const { greet, load_file } = res = await wasm;
-  console.log(res, load_file);
+  const { load_file } = await wasm;
+
+  // Given a File object, read it, pass it into wasm to be converted, resolve a blob 
+  function convertFile(file) {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onloadend = e => {
+        try {
+
+          const brs = load_file(reader.result);
+          if(!brs.length) {
+            console.error('Error converting bls');
+            reject(new Error('No Bricks'));
+            return;
+          }
+
+          resolve(new Blob([brs], { type: 'octet/stream' }));
+        } catch (e) {
+          console.error('Error converting bls', e)
+          reject(e);
+        }
+      };
+
+      reader.readAsBinaryString(file);
+    })
+  }
+
+
+  // Click on a magical link to download a file
+  function downloadFile(blob, filename) {
+    const elem = document.createElement('a');
+    document.body.appendChild(elem);
+    elem.style.display = 'none';
+
+    const objUrl = elem.href = window.URL.createObjectURL(blob);
+    elem.download = filename;
+    elem.click();
+
+    window.URL.revokeObjectURL(objUrl);
+    document.body.removeChild(elem);
+  }
 
   $('#blsForm').addEventListener('submit', event => {
     event.preventDefault();
 
-    const reader = new FileReader();
-    reader.onloadend = e => {
-      load_file(reader.result);
-    };
-
     const fileInput = event.target.file;
-    reader.readAsBinaryString(fileInput.files[0]);
+
+    const promises = Array.from(fileInput.files)
+      .map(f => {
+        console.log('Converting', f.name);
+        const newName = f.name.replace(/(.bls|)$/, '.brs');
+        convertFile(f)
+          .then(blob =>
+            downloadFile(blob, newName)
+          )
+          .catch(e => {
+            console.error('oops', e);
+            return Promise.resolve();
+          });
+      });
+
+    Promise.all(promises)
+      .then(() => {
+        console.log('done!');
+      })
   });
 });
 
