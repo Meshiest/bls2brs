70e34bfd5e11f9098b4d36a640cb4b34d43b16d8
Meshiest <meek.mesh@gmail.com>
2019-09-12 23:21:03 -0500

    Back to binary string

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,14 @@ use wasm_bindgen::prelude::*;
 use bls2brs::{bl_save, brs, convert};
 
 #[wasm_bindgen]
-pub fn load_file(body: &[u8]) -> std::vec::Vec<u8> {
-    let reader = bl_save::Reader::new(body).unwrap();
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn load_file(body: &str) -> std::vec::Vec<u8> {
+    let reader = bl_save::Reader::new(body.as_bytes()).unwrap();
     if let Ok(bls) = convert(reader) {
         let mut writer = vec![];
         brs::write_save(&mut writer, &bls.write_data).unwrap();
